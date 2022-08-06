0a126ccddfe7b9b36d7d5e671369b390a177b5f5
ashankritwik07 <110737148+ashankritwik07@users.noreply.github.com>
2022-08-06 22:45:00 +0530

    Question based on print statement

---
+++ print.java
@@ -0,0 +1,11 @@
+package MyClass;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****\n   *\n  *\n *\n*****");
+
+	}
+
+}
