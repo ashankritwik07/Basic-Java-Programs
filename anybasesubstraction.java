c7168bef9f26fab33d13a62ecaf096d5c370d8da
ashankritwik07 <110737148+ashankritwik07@users.noreply.github.com>
2022-09-04 14:46:09 +0530

    Update anybasesubstraction.java

--- anybasesubstraction.java
+++ anybasesubstraction.java
@@ -1,3 +1,11 @@
+/*
+Question :-
+1. You are given a base b.
+2. You are given two numbers n1 and n2 of base b.
+3. You are required to subtract n1 from n2 and print the value.
+*/
+
+
 package MyClass;
 import java.util.Scanner;
 public class anybasesubstraction {
