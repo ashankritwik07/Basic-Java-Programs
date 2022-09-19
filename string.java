6de87781bbc564a787ed54d82b91b963f105d5fd
ashankritwik07 <110737148+ashankritwik07@users.noreply.github.com>
2022-09-19 13:43:36 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,33 @@
+package MyClass;
+import java.util.Scanner;
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		//String s1 = sc.nextLine();
+      //System.out.println(s1);
+		//String s1 = sc.nextLine();
+		//System.out.println(s1);
+		//System.out.println(s1.length());
+		//char ch = s1.charAt(2);
+		//System.out.println(ch);
+	
+		//SUBSTRING:-
+		//String s = "abcdef";
+		//System.out.println(s.substring(1));
+		//System.out.println(s.substring(0,6));
+      
+      
+	}
+
+}
+/*
+ * in this project we learned about:-
+ * how to declear string;
+ * how to take input ib string;
+ * how to know the length of any string;
+ * how to print any char ;
+ * no setchar is possible;
+ * substring;
+ */
