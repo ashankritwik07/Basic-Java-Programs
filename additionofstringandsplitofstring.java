f250bf2250a805a8bb8c6b3d47df09e1c9d9e231
ashankritwik07 <110737148+ashankritwik07@users.noreply.github.com>
2022-09-20 13:23:30 +0530

    Add files via upload

---
+++ additionofstringandsplitofstring.java
@@ -0,0 +1,27 @@
+package MyClass;
+
+public class additionofstringandsplitofstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Addition of two String:-
+		//String s1 = "abc";
+		//String s2 = "def";
+		//String s3 = s1 +" "+ s2;
+		//System.out.println(s3);
+		
+		//Split of String:-
+		
+		//String s = " abc def ghi jkl mno ";
+		//String [] parts = s.split(" ");
+		//for(int i=0 ; i<parts.length ; i++) {
+			//System.out.println(parts[i]);
+		//}
+
+	}
+
+}
+/*
+ * 
+ */
